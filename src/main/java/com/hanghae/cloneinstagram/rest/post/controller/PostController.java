39d67f088764451dd16f76f7d717e5f5d454c7ff
Hwangbambi <26999725@naver.com>
2022-12-24 15:44:12 +0900

    #2 게시글 작성 및 파일 업로드

--- src/main/java/com/hanghae/cloneinstagram/rest/post/controller/PostController.java
+++ src/main/java/com/hanghae/cloneinstagram/rest/post/controller/PostController.java
@@ -1,4 +1,34 @@
 package com.hanghae.cloneinstagram.rest.post.controller;
 
+import com.hanghae.cloneinstagram.config.dto.PrivateResponseBody;
+import com.hanghae.cloneinstagram.config.errorcode.CommonStatusCode;
+import com.hanghae.cloneinstagram.config.security.UserDetailsImpl;
+import com.hanghae.cloneinstagram.rest.post.dto.PostRequestDto;
+import com.hanghae.cloneinstagram.rest.post.service.PostService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class PostController {
+
+    private final PostService postService;
+
+    @ApiOperation(value = "게시글 작성 및 파일 업로드")
+    @PostMapping("/posts")
+    public ResponseEntity<PrivateResponseBody> savePost(@ModelAttribute PostRequestDto postRequestDto) throws IOException {
+
+        return ResponseEntity.ok(new PrivateResponseBody(CommonStatusCode.CREATE_POST,postService.savePost(postRequestDto)));
+    }
+
+
 }
--- src/main/java/com/hanghae/cloneinstagram/rest/post/dto/PostRequestDto.java
+++ src/main/java/com/hanghae/cloneinstagram/rest/post/dto/PostRequestDto.java
@@ -1,4 +1,13 @@
 package com.hanghae.cloneinstagram.rest.post.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@NoArgsConstructor
 public class PostRequestDto {
+    private String content;
+    private MultipartFile file;
 }
