55cb1ebf7f1926b3b0b813ab739215082eeb99cd
Youngju Jang <joj1043@nate.com>
2022-12-25 20:52:31 +0900

    #5 카카오 로그인 구현 - 비회원의 경우 회원가입까지 동시에 되도록 구현

--- src/main/java/com/hanghae/cloneinstagram/rest/user/controller/UserController.java
+++ src/main/java/com/hanghae/cloneinstagram/rest/user/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.hanghae.cloneinstagram.rest.user.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.hanghae.cloneinstagram.config.dto.PrivateResponseBody;
 import com.hanghae.cloneinstagram.config.errorcode.StatusCode;
 import com.hanghae.cloneinstagram.config.errorcode.UserStatusCode;
+import com.hanghae.cloneinstagram.config.jwt.JwtUtil;
 import com.hanghae.cloneinstagram.rest.user.dto.LoginRequestDto;
 import com.hanghae.cloneinstagram.rest.user.dto.LoginResponseDto;
 import com.hanghae.cloneinstagram.rest.user.dto.SignupRequestDto;
 import com.hanghae.cloneinstagram.rest.user.repository.UserRepository;
+import com.hanghae.cloneinstagram.rest.user.service.KakaoService;
 import com.hanghae.cloneinstagram.rest.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -18,11 +21,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -35,6 +36,7 @@ public class UserController {
      private final PasswordEncoder passwordEncoder;
      private final UserRepository userRepository;
      private final UserService userService;
+     private final KakaoService kakaoService;
      
      @Operation(summary = "회원가입", description = "email, username, password 로 회원가입 ")
      @ApiResponses(value={
@@ -51,5 +53,13 @@ public class UserController {
      @PostMapping ("/login")
      public PrivateResponseBody<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
           return new PrivateResponseBody(UserStatusCode.USER_LOGIN_SUCCESS, userService.login(loginRequestDto, response));
+     }
+     
+     //https://kauth.kakao.com/oauth/authorize?client_id=ced49bfdb65f5f152e2e43f12e88bd86&redirect_uri=http://3.34.98.133/api/user/kakao/callback&response_type=code
+     //https://kauth.kakao.com/oauth/authorize?client_id=ced49bfdb65f5f152e2e43f12e88bd86&redirect_uri=http://localhost:8080/api/user/kakao/callback&response_type=code
+     @Operation(summary = "카카오 로그인 콜백", description = "email, password 로 로그인")
+     @GetMapping ("/kakao/callback")
+     public PrivateResponseBody<LoginResponseDto> kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+          return new PrivateResponseBody(UserStatusCode.USER_LOGIN_SUCCESS, kakaoService.kakaoLogin(code, response));
      }
 }
