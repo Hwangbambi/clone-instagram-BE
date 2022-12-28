e081a9dd8f7abc848cc697b18bc427fe7d8dce22
Youngju Jang <joj1043@nate.com>
2022-12-28 13:07:27 +0900

    #20 댓글 삭제 추가

--- src/main/java/com/hanghae/cloneinstagram/rest/comment/repository/CommentRepository.java
+++ src/main/java/com/hanghae/cloneinstagram/rest/comment/repository/CommentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByPostIdAndDeletedIsFalseOrderByCreatedAtDesc(Long postId);
@@ -29,4 +30,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
               "order by c.id desc "
     )
     List<CommentUsernameInterface> findByIdAndDeletedIsFalseOrderByCreatedAtDesc(Long postId);
+    
+    Optional<Comment> findByIdAndUserId(Long commentId, Long userId);
+    
 }
