8e322cbd2d07fd540644b54c998ec667b4c54204
Youngju Jang <joj1043@nate.com>
2022-12-29 16:01:09 +0900

    #26 내가 팔로우한 유저들의 게시글목록 >> + 나의 게시글도 나오도록 수정

--- src/main/java/com/hanghae/cloneinstagram/rest/post/repository/PostRepository.java
+++ src/main/java/com/hanghae/cloneinstagram/rest/post/repository/PostRepository.java
@@ -21,18 +21,18 @@ public interface PostRepository extends JpaRepository<Post, Long> {
                "join " +
                "(select id from " +  // 두개 union한거의 postId만 들고오기
                     "(select p.id from post p " +
-                    "inner join users u on p.user_id = u.id " + // 게시글과 게시글작성자 join
+                        "inner join users u on p.user_id = u.id " + // 게시글과 게시글작성자 join
                     "where p.deleted is false and u.username like %:search% " + // 삭제안된 게시글, 작성자명이 검색어와 일치
                     "order by p.id desc ) as A " +
                "union " +
                     "select id from " +
                     "(select p.id from post p " +
-                    "inner join hashtag h on p.id = h.post_id " + // 게시글과 해시태그의 게시글 join
+                       "inner join hashtag h on p.id = h.post_id " + // 게시글과 해시태그의 게시글 join
                     "where p.deleted is false and h.hashtag like %:search% " + // 삭제안된 게시글, 해당게시글의 해시태그가 검색어와 일치
                     "order by p.id desc ) as B " +
                "order by id desc) postId on post.id = postId.id " + // search에 해당되는 postId에 해당하는 post만 들고오기
-               "join users u2 on post.user_id = u2.id " + // 해당되는 게시글의 작성자정보 들고오기용
-               "left join post_like pl on post.id = pl.post_id and pl.user_id=:loggedUserId " + // 로그인한유저의 게시글좋아요유무 들고오기용
+                    "join users u2 on post.user_id = u2.id " + // 해당되는 게시글의 작성자정보 들고오기용
+                    "left join post_like pl on post.id = pl.post_id and pl.user_id=:loggedUserId " + // 로그인한유저의 게시글좋아요유무 들고오기용
                "where u2.deleted is false " +
                "order by id desc limit :size" )
      List<PostUsernameInterface> findAllByUsernameAndDeletedIsFalseOrderByIdDesc(int size, String search, @Param("loggedUserId")Long loggedUserId);
@@ -46,8 +46,8 @@ public interface PostRepository extends JpaRepository<Post, Long> {
           nativeQuery = true,
           value = "select post.*, u.username, u.profile_url, pl.user_id as isLike  " +
                "from post " +
-               "join users u on post.user_id = u.id " +
-               "left join post_like pl on post.id = pl.post_id and pl.user_id=:loggedUserId " +
+                    "join users u on post.user_id = u.id " +
+                    "left join post_like pl on post.id = pl.post_id and pl.user_id=:loggedUserId " +
                "where post.deleted is false and u.deleted is false " +
                "order by id desc limit :size" )
      List<PostUsernameInterface> findAllByDeletedIsFalseAndByUserOrderByIdDesc(@Param("size")int size, @Param("loggedUserId")Long loggedUserId);
@@ -57,8 +57,8 @@ public interface PostRepository extends JpaRepository<Post, Long> {
           nativeQuery = true,
           value = "select post.*, u.username, u.profile_url, pl.user_id as isLike " +
                "from post " +
-               "join users u on post.user_id = u.id " +
-               "left join post_like pl on post.id = pl.post_id and pl.user_id=:loggedUserId " +
+                    "join users u on post.user_id = u.id " +
+                    "left join post_like pl on post.id = pl.post_id and pl.user_id=:loggedUserId " +
                "where post.id = :postId and post.deleted is false and u.deleted is false")
      Optional<PostUsernameInterface> findByIdAndDeletedIsFalseAndByUserOrderByIdDesc(@Param("postId")Long postId, @Param("loggedUserId")Long loggedUserId);
      
@@ -69,8 +69,8 @@ public interface PostRepository extends JpaRepository<Post, Long> {
           nativeQuery = true,
           value = "select post.*, u.username, u.profile_url, pl.user_id as isLike  " +
                "from post " +
-               "join users u on post.user_id = u.id " +
-               "join post_like pl on post.id = pl.post_id and pl.user_id=:loggedUserId " +
+                    "join users u on post.user_id = u.id " +
+                    "join post_like pl on post.id = pl.post_id and pl.user_id=:loggedUserId " +
                "where post.deleted is false and u.deleted is false " +
                "order by id desc limit :size" )
      List<PostUsernameInterface> findAllByDeletedIsFalseAndByUserAndUserLikeOrderByIdDesc(@Param("size")int size,  @Param("loggedUserId")Long loggedUserId);
@@ -80,9 +80,9 @@ public interface PostRepository extends JpaRepository<Post, Long> {
           nativeQuery = true,
           value = "select  p.*, u.username, u.profile_url, pl.user_id as isLike  " +
                "from post p " +
-               "join follow f on f.user_id= :loggedUserId and p.user_id = f.follow_id " + // 로그인유저가 팔로우한 사람들
-               "left join post_like pl on p.id = pl.post_id and pl.user_id=:loggedUserId " + // 로그인유저의 게시글 좋아요유무
-               "join users u on p.user_id = u.id " + // 게시글 작성자 유저정보 용
+                    "join follow f on f.user_id= :loggedUserId and p.user_id = f.follow_id or p.user_id =1 " + // 로그인유저가 팔로우한 사람들
+                    "left join post_like pl on p.id = pl.post_id and pl.user_id=:loggedUserId " + // 로그인유저의 게시글 좋아요유무
+                    "join users u on p.user_id = u.id " + // 게시글 작성자 유저정보 용
                "where p.deleted is false and u.deleted is false " + // 삭제안된 게시글, 탈퇴안한사람의 게시글
                "order by p.id desc limit :size ") // 게시글 id이름 내림차순
      List<PostUsernameInterface> findByFollowedUser(@Param("size")int size,  @Param("loggedUserId")Long loggedUserId);
